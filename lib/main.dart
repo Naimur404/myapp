94ca4f41ee4583bb08d8c55455996030f15bee64
Naimur Rahman Nidrit <akash66445@gmail.com>
2022-02-21 16:39:13 +0600

    My First Flutter Project

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+  Material(
+color: Colors.lightBlueAccent,
+child:   Center(
+     child:
+      Text("Hello Worldr", textDirection: TextDirection.ltr, 
+      style: TextStyle(color: Colors.yellowAccent, fontSize: 50.0),
+      
+      )
+    ),
+
+
+  )
+
+
+
+
+  );
+}
+
